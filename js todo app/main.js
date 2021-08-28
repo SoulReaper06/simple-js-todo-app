751bd5864ec0822340f51c7247069a18841fa196
Jonat J Mathew <johnatj33@gmail.com>
2021-08-29 00:12:07 +0530

    Add files via upload

---
+++ js todo app/main.js
@@ -0,0 +1,31 @@
+let input = prompt("What would you like to do?");
+const todos = ['Collect chicken Eggs' , 'Clean Litter Box'];
+
+while(input !== 'quit' && input !== 'q') {
+    if(input === 'list'){
+      console.log('*****************');
+      for (let i = 0; i < todos.length; i++) {
+          console.log(`${i}: ${todos[i]}`);
+      }
+      console.log('*****************');
+
+    } else if ( input === 'new') {
+
+      const newTodo = prompt('Ok, what is the new todo');
+      todos.push(newTodo);
+      console.log(`${newTodo} added to the list!`);
+
+    } else if ( input === 'delete') {
+
+      const index = parseInt(prompt("Ok , enter the index to delete"));
+      const deleted = todos.splice(index, 1);
+      console.log(`Removed ${deleted[0]}`);
+    }
+
+
+
+  input = prompt("what would you like to do");
+
+}
+
+console.log("OK QUIT THE APP");
